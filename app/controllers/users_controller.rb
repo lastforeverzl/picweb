f443208002467d9676067a97ab8903b511212afc
lastforeverzl <lastforeverzl@gmail.com>
2013-08-07 23:44:14 -0400

    correct_user and index in users view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApplicationController
 
-	before_filter :signed_in_user, only:[:show]
+	before_filter :signed_in_user, only:[:index,:edit,:update]
+	before_filter :correct_user,   only: [:edit, :update]
+
+	def index
+		@users = User.all
+	end
 
 	def new
 		@user = User.new
@@ -23,11 +28,11 @@ class UsersController < ApplicationController
   	end
 
   	def edit
-  		@user = User.find(params[:id])
+  		# @user = User.find(params[:id])
   	end
 
   	def update
-	    @user = User.find(params[:id])
+	    # @user = User.find(params[:id])
 	    if @user.update_attributes(params[:user])
 	    	flash[:success] = "Profile updated"
 	      	sign_in @user
