1a25f31fb077b92d0f46d6cb27137415678f3ff3
lastforeverzl <lastforeverzl@gmail.com>
2013-08-09 18:42:07 -0400

    store image in dropbox

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ gem 'bcrypt-ruby', '3.0.1'
 gem 'will_paginate', '3.0.3'
 
 gem 'bootstrap-sass', '~> 2.3.2.1'
+
+gem "paperclip-dropbox",">=1"
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,17 @@
 class Project < ActiveRecord::Base
   # attr_accessible :title, :body
   attr_accessible :name, :position,:description
-
   attr_accessible :avatar
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
-  					:path => ":rails_root/public/assets/images/:id/:style/:filename",
-  					:url => "/assets/images/:id/:style/:filename"
+
+  # 	store pic in local folder
+  # has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  # 					:path => ":rails_root/public/assets/images/:id/:style/:filename",
+  # 					:url => "/assets/images/:id/:style/:filename"
+  # validates_attachment_size :avatar, :in => 0.megabytes..2.megabytes
+
+  has_attached_file :avatar,
+    :storage => :dropbox,
+    :styles => { :medium => "300x300>", :thumb => "100x100>" },
+    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+    :path => ":style/:id_:filename"
 end
